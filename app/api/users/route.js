2c74266a597edebaa8832deba9ba2f52ed877560
GTErickson <grant.erickson115@yahoo.com>
2024-04-20 16:45:42 -0700

    Sign up stuff

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,19 +1,39 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma"
+import { PrismaClientKnownRequestError } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 
 // register user endpoint
 export async function POST(req) {
-  const body = await req.json();
+  try {
+    const body = await req.json();
 
-  const newUser = await prisma.User.create({
-    data: {
-      name: body.name,
-      bio: body.bio,
-      password: body.password,
-      email: body.email
-    },
-  });
-  // TODO: Redirect  req.session.user = newUser;
+    // Validate request body
+    if (!body.name || !body.email || !body.password) {
+      throw new Error("Name, email, and password are required.");
+    }
 
-  return NextResponse.json({user: newUser.name}, {status: 200});
+    const newUser = await prisma.user.create({
+      data: {
+        name: body.name,
+        bio: body.bio,
+        password: body.password,
+        email: body.email,
+      },
+    });
+
+    return NextResponse.json({ user: newUser.name }, { status: 200 });
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === "P2002" &&
+      error.meta.target.includes("email")
+    ) {
+      // Email already exists in the database
+      return NextResponse.error("Email already exists", { status: 400 });
+    } else {
+      // Other errors
+      console.error("Error registering user:", error);
+      return NextResponse.error("Internal Server Error", { status: 500 });
+    }
+  }
 }
