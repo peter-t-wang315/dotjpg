4016aaa8aa4eec99d7be809fac4e3ab4d5f509c7
peter-t-wang315 <wang.t.peter@gmail.com>
2024-04-20 18:46:21 -0700

    Pulling main again

--- app/review/[setID]/page.js
+++ app/review/[setID]/page.js
@@ -108,6 +108,7 @@ export default function Index({ params }) {
             rating={review.stars}
             review={review.review}
             createdAt={dateToString(review.createdAt)}
+            isAdmin={isAdmin}
           />
         ))
       ) : (
--- components/SetReviewBlock.js
+++ components/SetReviewBlock.js
@@ -4,14 +4,16 @@ export default function SetReviewBlock({
   username,
   rating,
   review,
-  createdAt
+  createdAt,
+  isAdmin,
 }) {
   return (
     <div className="card-primary flex-row w-full p-0">
-      <div className="flex flex-col bg-background-darker w-1/6 pt-2 pb-3 px-3 rounded-l-md">
+      <div className="flex flex-col bg-background-darker w-1/6 pt-2 pb-3 px-3 rounded-l-md gap-2">
         <h4>{username}</h4>
         <p>{createdAt}</p>
         <RatingStars numStars={rating} />
+        {isAdmin && <button className="btn-primary self-start">Delete</button>}
       </div>
       <div className="flex w-5/6 p-2">{review}</div>
     </div>
